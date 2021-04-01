0d75b6f0ba89925661bbadf0eabd5c60ce40cd21
AntonDeXy <podolyananton@gmail.com>
2021-04-01 13:15:59 +0300

    webpack and gulp: add loaders for scss

---
+++ webpack_gulp/webpack-template/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  // resolve: {
+  //   extensions: ['.js', '.jsx', '.css', '.scss'],
+  //   modules: [
+  //     'node_modules'
+  //   ]        
+  // }
+}
